112d72a865ab7f02de4ece55fb7dca4225013d3c
DenebTM <deneb@screee.ee>
2023-12-09 21:08:13 +0100

    use StatementBuilder to construct test program

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,24 @@ mod functions;
 mod program;
 mod statement;
 
-use block::TestBlock;
 use expression::{AExp::*, BExp::*};
-use statement::{boxed::*, Statement::Composition};
 
-use crate::program::Program;
+use crate::{program::Program, statement::builder::StatementBuilder};
 
 fn main() {
-    let program = Program::new(Composition(
-        assignment(0, 0, Number(2)),
-        composition(
-            assignment(0, 1, Number(4)),
-            composition(
-                assignment(0, 0, Number(1)),
-                composition(
-                    if_then_else(
-                        TestBlock {
-                            label: 0,
-                            expr: RelationalOp(&Variable(1), ">", &Variable(0)),
-                        },
-                        assignment(0, 2, Variable(1)),
-                        assignment(0, 2, ArithmeticOp(&Variable(1), "*", &Variable(1))),
-                    ),
-                    assignment(0, 0, Variable(2)),
-                ),
-            ),
-        ),
-    ));
+    let program = Program::new(
+        StatementBuilder::new(1)
+            .assignment(0, Number(2))
+            .assignment(1, Number(4))
+            .assignment(0, Number(1))
+            .if_then(RelationalOp(&Variable(1), ">", &Variable(0)))
+            .assignment(2, Variable(1))
+            .else_()
+            .assignment(2, ArithmeticOp(&Variable(1), "*", &Variable(1)))
+            .end()
+            .assignment(0, Variable(2))
+            .end(),
+    );
 
     println!("{:?}", program.flow_r());
 
