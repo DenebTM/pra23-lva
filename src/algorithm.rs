ef3a3421c46704eff50ede261e651c1cb49bd90e
DenebTM <deneb@screee.ee>
2023-12-09 18:49:31 +0100

    actually use extremal{ value, label} and bottom

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -26,7 +26,7 @@ pub mod chaotic_iter {
 
 pub mod mfp {
     use crate::{
-        analysis::{lv_entry, lv_entry_at, LVAnalysis, LVEntry, LVExit},
+        analysis::{lv_entry, lv_entry_at, LVAnalysis, LVExit},
         expression::{Label, Variable},
         program::Program,
     };
@@ -40,19 +40,29 @@ pub mod mfp {
         let ext_lab: HashSet<Label> = [program.init_label()].into();
         let ext_val: Lattice = [].into();
         let flow = program.flow_r();
-        let f_l = |p: &Program, a: &LVExit, l: Label| lv_entry_at(p, a, l);
+        let f_l = |p: &Program, a: &LVExit| lv_entry(p, a);
+        let f_l_at = |p: &Program, a: &LVExit, l: Label| lv_entry_at(p, a, l);
 
         // step 1: initialize
         let mut work_list: Vec<(Label, Label)> = flow.iter().cloned().collect();
         let mut ana: Analysis = (1..=program.len)
-            .map(|label| (label, HashSet::new()))
+            .map(|label| {
+                (
+                    label,
+                    if ext_lab.contains(&label) {
+                        ext_val.clone()
+                    } else {
+                        bottom.clone()
+                    },
+                )
+            })
             .collect();
 
         // step 2: iterate
         while work_list.len() > 0 {
             let (l, l_p) = work_list.remove(0);
 
-            let (a, b) = (f_l(program, &ana, l), &ana[&l_p]);
+            let (a, b) = (f_l_at(program, &ana, l), &ana[&l_p]);
             if !a.is_subset(&b) {
                 ana.insert(l_p, [a, b.clone()].iter().flatten().cloned().collect());
 
@@ -67,7 +77,7 @@ pub mod mfp {
         // step 3: present
         LVAnalysis {
             exit: ana.clone(),
-            entry: lv_entry(program, &ana),
+            entry: f_l(program, &ana),
         }
     }
 }
