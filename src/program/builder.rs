32a5579d14bea2b894a159a04bbbceeea7f5568f
DenebTM <deneb@screee.ee>
2023-12-09 21:05:52 +0100

    if/else builder

--- src/program/builder.rs
+++ src/program/builder.rs
@@ -41,6 +41,122 @@ impl<'a> StatementBuilder<'a> {
             next_label: self.next_label + 1,
         }
     }
+
+    pub fn if_then(self, test: BExp<'a>) -> IfThenBuilder<'a> {
+        IfThenBuilder::new(test, self)
+    }
+
+    pub fn end(self) -> Statement<'a> {
+        self.contents
+    }
+
+    fn append(self, stmt: Statement<'a>, next_label: Label) -> Self {
+        Self {
+            contents: append(self.contents, stmt),
+            next_label,
+        }
+    }
+}
+
+pub struct IfThenBuilder<'a> {
+    test: BExp<'a>,
+    then_builder: StatementBuilder<'a>,
+    parent: StatementBuilder<'a>,
+}
+impl<'a> IfThenBuilder<'a> {
+    fn new(test: BExp<'a>, super_builder: StatementBuilder<'a>) -> Self {
+        let mut inst = Self {
+            test,
+            then_builder: StatementBuilder::new(0),
+            parent: super_builder,
+        };
+        inst.then_builder.next_label = inst.parent.next_label;
+
+        inst
+    }
+
+    pub fn assignment(self, var: Variable, expr: AExp<'a>) -> Self {
+        Self {
+            test: self.test,
+            then_builder: self.then_builder.assignment(var, expr),
+            parent: self.parent,
+        }
+    }
+
+    pub fn skip(self) -> Self {
+        Self {
+            test: self.test,
+            then_builder: self.then_builder.skip(),
+            parent: self.parent,
+        }
+    }
+
+    pub fn test(self, expr: BExp<'a>) -> Self {
+        Self {
+            test: self.test,
+            then_builder: self.then_builder.test(expr),
+            parent: self.parent,
+        }
+    }
+
+    pub fn else_(self) -> ElseBuilder<'a> {
+        ElseBuilder::new(self)
+    }
+}
+//
+
+pub struct ElseBuilder<'a> {
+    if_builder: IfThenBuilder<'a>,
+    else_builder: StatementBuilder<'a>,
+}
+impl<'a> ElseBuilder<'a> {
+    fn new(if_builder: IfThenBuilder<'a>) -> Self {
+        let mut inst = Self {
+            if_builder,
+            else_builder: StatementBuilder::new(0),
+        };
+        inst.else_builder.next_label = inst.if_builder.then_builder.next_label;
+
+        inst
+    }
+
+    pub fn assignment(self, var: Variable, expr: AExp<'a>) -> Self {
+        Self {
+            if_builder: self.if_builder,
+            else_builder: self.else_builder.assignment(var, expr),
+        }
+    }
+
+    pub fn skip(self) -> Self {
+        Self {
+            if_builder: self.if_builder,
+            else_builder: self.else_builder.skip(),
+        }
+    }
+
+    pub fn test(self, expr: BExp<'a>) -> Self {
+        Self {
+            if_builder: self.if_builder,
+            else_builder: self.else_builder.test(expr),
+        }
+    }
+
+    pub fn end(self) -> StatementBuilder<'a> {
+        let test_label = self.if_builder.parent.next_label;
+        let next_label = self.else_builder.next_label;
+
+        self.if_builder.parent.append(
+            Statement::IfThenElse(
+                TestBlock {
+                    label: test_label,
+                    expr: self.if_builder.test,
+                },
+                Box::new(self.if_builder.then_builder.end()),
+                Box::new(self.else_builder.end()),
+            ),
+            next_label,
+        )
+    }
 }
 
 fn append<'a>(stmt: Statement<'a>, next: Statement<'a>) -> Statement<'a> {
