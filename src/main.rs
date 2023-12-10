279b991bb99c982958146e52784e348cd1f2b3ae
DenebTM <deneb@screee.ee>
2023-12-10 23:51:18 +0100

    prepare for hasty submission

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,61 @@ mod parser;
 mod program;
 mod statement;
 
-use expression::{AExp::*, BExp::*};
+use std::{env, io};
 
-use crate::{parser::parse, program::Program, statement::builder::StatementBuilder};
+use crate::parser::parse;
+
+// needed by the example program
+use crate::{
+    expression::{AExp::*, BExp::*},
+    program::Program,
+    statement::builder::StatementBuilder,
+};
 
 fn main() {
-    let program = Program::new(
-        StatementBuilder::new(1)
-            .assignment(0, Number(2))
-            .assignment(1, Number(4))
-            .assignment(0, Number(1))
-            .begin_if(RelationalOp(Variable(1), ">".to_string(), Variable(0)))
-            .assignment(2, Variable(1))
-            .else_()
-            .assignment(
-                2,
-                ArithmeticOp(
-                    Box::new(Variable(1)),
-                    "*".to_string(),
-                    Box::new(Variable(1)),
-                ),
-            )
-            .end_if()
-            .assignment(0, Variable(2))
-            .end(),
-    );
+    let args: Vec<String> = env::args().collect();
 
-    println!("{}", program);
-    println!("{:?}", program.flow_r());
+    println!("Enter statements here! An example program can be found at ./example_program");
+    println!("To use an input file: Run {} < (input file name)", args[0]);
+
+    let stdin = io::stdin();
+    let mut input = String::new();
+    let mut buf = String::new();
+    while let Ok(count) = stdin.read_line(&mut buf) {
+        if count == 0 {
+            break;
+        }
+
+        input.push_str(&buf);
+        buf.clear();
+    }
+
+    let program = parse(&input.trim());
+
+    // uncomment this if for some reason the input doesn't work
+    // let program = new(
+    //     StatementBuilder::new(1)
+    //         .assignment(0, Number(2))
+    //         .assignment(1, Number(4))
+    //         .assignment(0, Number(1))
+    //         .begin_if(RelationalOp(Variable(1), ">".to_string(), Variable(0)))
+    //         .assignment(2, Variable(1))
+    //         .else_()
+    //         .assignment(
+    //             2,
+    //             ArithmeticOp(
+    //                 Box::new(Variable(1)),
+    //                 "*".to_string(),
+    //                 Box::new(Variable(1)),
+    //             ),
+    //         )
+    //         .end_if()
+    //         .assignment(0, Variable(2))
+    //         .end(),
+    // );
+
+    println!("Program: {}", program);
+    println!("Flow: {:?}", program.flow_r());
     println!();
 
     // let lva = algorithm::chaotic_iter::run(&program);
@@ -46,10 +73,4 @@ fn main() {
             lva.entry[&label], lva.exit[&label],
         )
     }
-
-    parse(
-        "
-    x := 2; y := 4; x := 1; if y > x then z := y else z := y*y endif;
-    x := z",
-    );
 }
