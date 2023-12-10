67ef248e3548ae7c66e115aeead9b646e0cd9862
DenebTM <deneb@screee.ee>
2023-12-10 23:52:48 +0100

    add note about Ctrl+D

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("Enter statements here! An example program can be found at ./example_program");
+    println!("To finish the program, press Ctrl+D");
     println!("To use an input file: Run {} < (input file name)", args[0]);
 
     let stdin = io::stdin();
